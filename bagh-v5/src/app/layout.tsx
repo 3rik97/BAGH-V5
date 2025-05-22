268a78fa8faa4a58b2938f41d00ae5fa65cedcb5
3rik97 <151696015+3rik97@users.noreply.github.com>
2025-05-22 12:04:50 +0800

    Update layout.tsx

--- bagh-v5/src/app/layout.tsx
+++ bagh-v5/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter, Nunito_Sans } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,6 +24,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "BAGH Casino Online | Bangladesh’s Trusted Betting",
   description: "BAGH Casino brings smart, secure online betting to Bangladesh. Enjoy fair play, local support, and high-quality slots and live games with Bagh8.",
+  other: {
+    'google-site-verification': 'yraiRxDBM2pGvft8s6LGwfXWPVAXAKwsw8rIiPFvsVo',
+  },
 };
 
 export default function RootLayout({
@@ -33,12 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="yraiRxDBM2pGvft8s6LGwfXWPVAXAKwsw8rIiPFvsVo"
-        />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${Nunito.variable} antialiased`}
       >
