aaa37e28fef3ba378e37f9a20a0dab5193510161
3rik97 <151696015+3rik97@users.noreply.github.com>
2025-06-09 14:24:29 +0800

    Update layout.tsx

--- bagh-v5/src/app/layout.tsx
+++ bagh-v5/src/app/layout.tsx
@@ -36,6 +36,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZH45BM41D0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-ZH45BM41D0');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${Nunito.variable} antialiased`}
       >
