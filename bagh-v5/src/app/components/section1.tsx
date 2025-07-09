4dc2377eaa9d7ad8f3ca459809b76eeb97e5f40c
lostrobin <lost.robin@auroramy.com>
2025-07-09 22:42:12 +0800

    replace PNG image with WEBP format

--- bagh-v5/src/app/components/section1.tsx
+++ bagh-v5/src/app/components/section1.tsx
@@ -5,16 +5,14 @@ export default function Section() {
     <section className="section1" id="#">
       <div className="relative pt-[80px] m-auto text-white  max-w-[1440px]">
         <Image
-          src="/Dice separated.png"
+          src="/Dice separated.webp"
           alt="dice"
           width={800}
           height={600}
-          className="w-[80vw] sm:w-[35vw] m-auto"
-          priority={true}
+          className="w-[80vw] sm:w-[35vw] m-auto "
+          quality={100}
+          priority
           fetchPriority="high"
-          sizes="(max-width: 640px) 80vw, 35vw"
-          placeholder="blur"
-          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCdABmX/9k="
         />
         <div className="bottom-0 flex flex-col justify-center gap-2">
           <h1 className="text-center font-extrabold text-[36px] leading-[44px] text-[#E8E6E3] font-['Inter']">
