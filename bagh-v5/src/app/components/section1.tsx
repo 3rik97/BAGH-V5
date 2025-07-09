198c9e501b04bfee97845986ee7ccfed8b278f2c
lostrobin <lost.robin@auroramy.com>
2025-07-09 22:30:44 +0800

    refactor Image component

--- bagh-v5/src/app/components/section1.tsx
+++ bagh-v5/src/app/components/section1.tsx
@@ -9,9 +9,12 @@ export default function Section() {
           alt="dice"
           width={800}
           height={600}
-          className="w-[80vw] sm:w-[35vw] m-auto "
-          priority
+          className="w-[80vw] sm:w-[35vw] m-auto"
+          priority={true}
           fetchPriority="high"
+          sizes="(max-width: 640px) 80vw, 35vw"
+          placeholder="blur"
+          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwCdABmX/9k="
         />
         <div className="bottom-0 flex flex-col justify-center gap-2">
           <h1 className="text-center font-extrabold text-[36px] leading-[44px] text-[#E8E6E3] font-['Inter']">
